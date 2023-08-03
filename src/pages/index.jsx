33e5706f135864f38de4db7d890a98320a5e7847
PeppeTll <peppetll@gmail.com>
2023-08-03 16:06:59 +0200

    feat: header comp in index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,9 @@ import styles from "@/styles/Home.module.scss";
 import hero from "@/assets/heroes/hero.jpeg";
 import banner from "@/assets/images/banner.jpeg";
 
+//components
+import Header from "@/components/header";
+
 export default function Home() {
 	return (
 		<>
@@ -15,7 +18,9 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className={styles.main}></main>
+			<main className={styles.main}>
+				<Header />
+			</main>
 		</>
 	);
 }
