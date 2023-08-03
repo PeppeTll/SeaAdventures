a18d7238db42d872c0acdc7923d66cd1eacce535
PeppeTll <peppetll@gmail.com>
2023-08-03 16:50:28 +0200

    feat: cardList comp

---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss";
+
+import data from "@/mock/data.js";
+
+const CardList = () => {
+	return <div className={styles.CardList}>{/* Card */}</div>;
+};
+
+export default CardList;
